075cb3253cd3edcca3c180251f70919cb20a3403
Desmond <desmond.dd31@gmail.com>
2018-03-03 15:31:00 +0530

    first init

---
+++ imports/ui/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import Task from './Task.js';
+import { Tasks } from '../api/tasks.js';
+import { withTracker } from 'meteor/react-meteor-data';
+import ReactDOM from 'react-dom';
+
+// App component - represents the whole app
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      hideCompleted:false,
+    }
+  }
+
+  // HIDE COMPLETED TASKS
+  toggleHideCompleted(){
+    this.setState({
+      hideCompleted:!this.state.hideCompleted,
+    })
+  }
+  // HIDE COMPLETED TASKS
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+  renderTasks() {
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted)
+    {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List {this.props.inCompleteTasks}</h1>
+
+          <label className="hide-completed">
+            <input type="checkbox" readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"/>
+          </form>
+        </header>
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default withTracker(()=> {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    inCompleteTasks:Tasks.find({checked:{$ne:true}}).count(),
+  };
+})(App);
+
+/*
+MONGO_URL='mongodb://cluster-Admin:ClusterAdmin2895@cluster0-shard-00-00-u33j0.mongodb.net:27017,cluster0-shard-00-01-u33j0.mongodb.net:27017,cluster0-shard-00-02-u33j0.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin/meteorReactTodos' meteor
+*/
---
+++ imports/ui/Task.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Tasks } from '../api/tasks.js';
+
+export default class Task extends Component {
+  toggleChecked() {
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
+  render() {
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+    return (
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={!!this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+          {this.props.task.text}
+      </li>
+    );
+  }
+}
